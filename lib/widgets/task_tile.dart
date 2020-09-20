2e0bae719e0ea70f79c718761c1e465371ae08c1
Vasu Goel <thevasugoel@gmail.com>
2020-09-21 00:37:30 +0530

    Created checkbox callbacks and use the state on upper level and use them in children Widgets

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool checkboxState = false;
+
+  void checkboxCallback(newValue) {
+    setState(() {
+      checkboxState = newValue;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task.'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool value) {},
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+            decoration: checkboxState ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckbox(checkboxState, checkboxCallback),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool isChecked;
+  final Function toggleCheckboxCallback;
+
+  TaskCheckbox(this.isChecked, this.toggleCheckboxCallback);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: toggleCheckboxCallback,
     );
   }
 }
